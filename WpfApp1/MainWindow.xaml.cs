f32104fde286148a4b131b5f5156c12ff92e129d
Зейтын Бахытбек Кылышбекович <@corp.itstep>
2018-01-16 14:56:46 +0600

    first

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            while (true)
+            {
+                string path = @"C:\Users\ЗейтынБ\Documents\Visual Studio 2017\Projects\users.json";
+                if (textbox1.Text == "")
+                {
+                    MessageBox.Show("Введите логин");
+                    return;
+                }
+                else if (password1.Password== "")
+                {
+                    MessageBox.Show("Введите пароль");
+                    return;
+                }
+                else
+                {
+                    string json="";     
+                    try
+                    {
+                        json= File.ReadAllText(path);
+                    }
+                    catch(Exception z)
+                    {
+                        MessageBox.Show("Зарегистрируйтесь пожалуйста!");
+                        textbox1.Text = "";
+                        password1.Password = "";
+                        return;
+                    }
+
+                    JArray jarray = JArray.Parse(json);
+
+                    for(int i=0; i<jarray.Count; i++)
+                    {
+                        if (jarray[i]["Name"].ToString() == textbox1.Text && jarray[i]["Password"].ToString() == password1.Password)
+                        {
+                            MessageBox.Show("Добро пожаловать");
+                            Window2 window = new Window2();
+                            window.label1.Content += " " + textbox1.Text;
+                            window.Show();
+                            this.Close();
+                            return;
+                        }          
+                    }
+                    MessageBox.Show("Неправильное имя или пароль");
+                    textbox1.Text = "";
+                    password1.Password = "";
+                    return;
+                }
+            }
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {  
+            while (true)
+            {
+                if (textbox3.Text == "")
+                {
+                    MessageBox.Show("Заполните все поля");
+                    break;
+                }
+                else if (password1.Password == "")
+                {
+                    MessageBox.Show("Заполните все поля");
+                    break;
+                }
+                else if (password2.Password == "")
+                {
+                    MessageBox.Show("Заполните все поля");
+                    break;
+                }
+                else if (password1.Password != password2.Password)
+                {
+                    MessageBox.Show("Пароли не совпадают");
+                    break;
+                }
+                else if(textbox6.Text == "")
+                {
+                    MessageBox.Show("Заполните все поля");
+                    break;
+                }
+                else if (image.Source == null)
+                {
+                    MessageBox.Show("Выберите фото, пожалуиста");
+                    break;
+                }
+                else if (textBox.Text == "")
+                {
+                    MessageBox.Show("Заполните все поля");
+                    break;
+                }
+                else
+                {
+                    string pattern = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+
+                    while (true)
+                    {
+                        string email = textbox6.Text;
+                        if (Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase))
+                        {
+                            string path = @"C:\Users\ЗейтынБ\Documents\Visual Studio 2017\Projects\users.json";
+                            if (!File.Exists(path))
+                            {
+                                List<User> user = new List<User>();
+                                user.Add(new User { Name = textbox3.Text, Password = password1.Password, Email = textbox6.Text });
+                                File.WriteAllText(path, JsonConvert.SerializeObject(user));
+                                MessageBox.Show("Поздравляю ваша региcтрация прошла успешно");
+                                textbox3.Text = "";
+                                password1.Password = "";
+                                password2.Password = "";
+                                textbox6.Text = "";
+                                textBox.Text = "";
+                                image.Source = null;
+                                return;
+                            }
+                            else
+                            {
+                                string json = File.ReadAllText(path);
+                                var deserialize = JsonConvert.DeserializeObject<List<User>>(json);
+                                foreach (var q in deserialize)
+                                {
+                                    if (q.Email == textbox6.Text)
+                                    {
+                                        MessageBox.Show("Пользователь уже существует");
+                                        textbox3.Text = "";
+                                        password1.Password = "";
+                                        password2.Password = "";
+                                        textbox6.Text = "";
+                                        textBox.Text = "";
+                                        image.Source = null;
+                                        return;
+                                    }
+                                }
+                                deserialize.Add(new User { Name = textbox3.Text, Password = password1.Password, Email = textbox6.Text });
+                                var jsonToOutput = JsonConvert.SerializeObject(deserialize, Formatting.Indented);
+                                File.WriteAllText(path, jsonToOutput);
+                                MessageBox.Show("Поздравляю ваша региcтрация прошла успешно");
+                                textbox3.Text = "";
+                                password1.Password = "";
+                                password2.Password = "";
+                                textbox6.Text = "";
+                                textBox.Text = "";
+                                image.Source = null;
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Некорректный email");
+                            textbox6.Text = "";
+                            return;
+                        }
+                    }
+                }              
+            }
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+
+            of.Filter = "png files (*.png)|*.png";
+
+            if (of.ShowDialog() == true)
+            {
+                BitmapImage bimage = new BitmapImage();
+                bimage.BeginInit();
+                bimage.UriSource = new Uri(of.FileName, UriKind.Absolute);
+                bimage.EndInit();
+                image.Source = bimage;
+            }
+
+        }
+    }
+
+}
